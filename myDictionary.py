d23dcf4f6ad5161ff7fa576044e332f7f6a8da12
Patrick Adoko Yawson <atrickpay40@gmail.com>
2025-05-03 14:12:33 +0000

    Add files via upload

---
+++ myDictionary.py
@@ -0,0 +1,15 @@
+# Create my dictionary
+
+myDictionary = {'name': 'Patrick A Yawson', 'age': 35, 'country': 'Ghana', 'city': 'Takoradi'}
+
+print(myDictionary.values())
+
+# check data type
+
+print(type(myDictionary))
+
+print(myDictionary['name'])
+print(myDictionary['age'])
+print(myDictionary['country'])
+print(myDictionary['city'])
+      
